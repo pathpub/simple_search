e112b82822b268075d67002a0f582e9af455726e
jimmybot <jimmy@path.pub>
2024-07-01 17:30:21 -0400

    Add types and clean up code a bit

--- lib/simplesearch.ex
+++ lib/simplesearch.ex
@@ -13,6 +13,9 @@ defmodule SimpleSearch do
     SimpleSearch.search(segment, "your search query")
   """
 
+  @type segment :: {map(), map()}
+
+  @spec initialize() :: {:aborted, any()} | {:atomic, :ok}
   def initialize() do
     :mnesia.stop()
     :mnesia.create_schema([node()])
@@ -21,22 +24,24 @@ defmodule SimpleSearch do
     :mnesia.create_table(InvertedIndexBigrams, attributes: [:term, :docs])
   end
 
+  @spec start() :: :ok | {:error, any()}
   def start() do
     :mnesia.start()
   end
 
+  @spec new_segment() :: segment()
   def new_segment() do
     {%{}, %{}}
   end
 
-  @doc """
-  """
+  @spec index_all(segment(), [{integer(), String.t()}]) :: segment()
   def index_all(segment, documents) do
     Enum.reduce(documents, segment, fn document, segment ->
       SimpleSearch.index_one(segment, document)
     end)
   end
 
+  @spec index_one(segment(), {integer(), String.t()}) :: segment()
   def index_one(segment, {doc_id, text}) do
     unigrams = doc2tokens(text)
     bigrams = Enum.chunk_every(unigrams, 2, 1, :discard)
@@ -62,6 +67,7 @@ defmodule SimpleSearch do
     {unigram_index, bigram_index}
   end
 
+  @spec doc2tokens(String.t()) :: [String.t()]
   def doc2tokens(document) do
     String.downcase(document)
     |> strip_punctuation()
@@ -71,24 +77,25 @@ defmodule SimpleSearch do
     end)
   end
 
+  @spec strip_punctuation(String.t()) :: String.t()
   def strip_punctuation(document) do
     String.replace(document, ~r/[[:punct:]]/, " ")
   end
 
-  def write_all(segment) do
-    {unigram_index, bigram_index} = segment
-
-    Enum.each(unigram_index, fn e ->
+  @spec write_all(segment()) :: :ok | {:error, any()}
+  def write_all({unigram_idx, bigram_idx} = _segment) do
+    Enum.each(unigram_idx, fn e ->
       :mnesia.dirty_write(InvertedIndex, e)
     end)
 
-    Enum.each(bigram_index, fn e ->
+    Enum.each(bigram_idx, fn e ->
       :mnesia.dirty_write(InvertedIndexBigrams, e)
     end)
   end
 
+  @spec read_all() :: segment() | {:aborted, any()}
   def read_all() do
-    unigram_index =
+    {:atomic, unigram_idx} =
       :mnesia.transaction(fn ->
         :mnesia.foldl(
           fn rec, acc ->
@@ -99,7 +106,7 @@ defmodule SimpleSearch do
         )
       end)
 
-    bigram_index =
+    {:atomic, bigram_idx} =
       :mnesia.transaction(fn ->
         :mnesia.foldl(
           fn rec, acc ->
@@ -110,7 +117,7 @@ defmodule SimpleSearch do
         )
       end)
 
-    {unigram_index, bigram_index}
+    {unigram_idx, bigram_idx}
   end
 
   # TODO: ranking based on term frequency and bigram matches
@@ -118,14 +125,7 @@ defmodule SimpleSearch do
   def search(segment, query) do
     {unigram_idx, bigram_idx} = segment
 
-    unigram_terms =
-      Enum.map(
-        String.downcase(query)
-        |> strip_punctuation
-        |> String.split(),
-        &Stemmer.stem/1
-      )
-
+    unigram_terms = doc2tokens(query)
     bigram_terms = Enum.chunk_every(unigram_terms, 2, 1, :discard)
 
     all_matches =
